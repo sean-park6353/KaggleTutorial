a94f8bab61e95ea4d1317133e98fd1b01fa288eb
unknown <package9@naver.com>
2020-07-22 00:17:31 +0900

    112

--- 1. Introduction.py
+++ 1. Introduction.py
@@ -3,12 +3,5 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 
-year = [1950, 1970, 1990, 2010, 2020]
-pop = [2.519, 3.692, 5.263, 6.972, 8.123]
-plt.plot(year, pop)
-# Adding Axis Labels
-plt.xlabel('Year')
-plt.ylabel('Population')
-plt.title('World Population Projections')
-# Show
-plt.show()
+year = [1950, 1970, 1990, 2010]
+pop
