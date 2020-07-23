b4973685a1b3fda4f40317cd361c164bc282eb1b
unknown <package9@naver.com>
2020-07-23 16:16:39 +0900

    pandas with

--- 1. Introduction.py
+++ 1. Introduction.py
@@ -3,11 +3,17 @@ import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 
-d = {'user': 'bozo', 'pswd': 1234}
-print(d['user'])
-del(d['user'])
-print(d)
-d.clear()
-print(d)
-d['id'] = 45
-print(d)
+raw_data = {
+    'first_name': ['park', 'kim', 'lim', np.nan, np.nan],
+    'nationality': ['koera', 'korea', 'France', 'UK', 'UK'],
+    'age': [28, 23, 25, 36, 70]
+}
+
+df = pd.DataFrame(raw_data)
+
+print(df)
+
+
+df1 = pd.DataFrame(raw_data, columns=['first_name', 'age', 'nationality'])
+
+print(df1)
