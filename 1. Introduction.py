e7cb1ef6d8a1d39e0e9d37dd8e252286feaad7d1
unknown <package9@naver.com>
2020-07-23 15:48:29 +0900

    123

--- 1. Introduction.py
+++ 1. Introduction.py
@@ -4,6 +4,10 @@ import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 
 d = {'user': 'bozo', 'pswd': 1234}
-print("123")
-print(d['pswd'])
+print(d['user'])
+del(d['user'])
+print(d)
+d.clear()
+print(d)
+d['id'] = 45
 print(d)
