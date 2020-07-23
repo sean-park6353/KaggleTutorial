23a589eb0c963709b55fc242fe258f25d439377e
unknown <package9@naver.com>
2020-07-23 17:30:20 +0900

    33

--- 1. Introduction.py
+++ 1. Introduction.py
@@ -1,19 +1,5 @@
-import pylab as pl
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
 
-raw_data = {
-    'first_name': ['park', 'kim', 'lim', np.nan, np.nan],
-    'nationality': ['koera', 'korea', 'France', 'UK', 'UK'],
-    'age': [28, 23, 25, 36, 70]
-}
+import pandas as pd
 
-df = pd.DataFrame(raw_data)
-
+df = pd.read_csv("./train.csv")
 print(df)
-
-
-df1 = pd.DataFrame(raw_data, columns=['first_name', 'age', 'nationality'])
-
-print(df1)
